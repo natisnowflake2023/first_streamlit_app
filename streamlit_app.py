fa758010c073bc60ea91ba2f2e5f2b067aa0a515
natisnowflake2023 <137405698+natisnowflake2023@users.noreply.github.com>
2023-06-22 10:33:49 -0400

    Update streamlit_app.py
    Modif 6

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multislect("Pick some fruits:", list(my_freuit_list.indez))
+streamlit.multislect("Pick some fruits:", list(my_freuit_list.index))
 streamlit.dataframe(my_fruit_list)
