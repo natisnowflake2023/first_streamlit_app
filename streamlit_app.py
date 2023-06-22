c0688267ea8795e2e1def7cb55c6fbaf4e8c2a07
natisnowflake2023 <137405698+natisnowflake2023@users.noreply.github.com>
2023-06-22 10:08:22 -0400

    Update streamlit_app.py
    Modif 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
-streamlit.title("My Parents New Hwalthy Diner")
-streamlit.title("Breakfast Menu")
-streamlit.text("Omega 3")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
