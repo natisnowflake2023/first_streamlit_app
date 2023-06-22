e39880c324228ddc31850091a389ea239c989220
natisnowflake2023 <137405698+natisnowflake2023@users.noreply.github.com>
2023-06-22 10:09:28 -0400

    Update streamlit_app.py
    Modif 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
