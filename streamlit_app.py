0c4ad66c888451c5507562a17e05de71b6a9456e
natisnowflake2023 <137405698+natisnowflake2023@users.noreply.github.com>
2023-06-26 14:45:55 -0400

    Update streamlit_app.py
    ññ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,15 @@ import pandas
 import streamlit
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+streamlit.write('Thanls for adding ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
+
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
