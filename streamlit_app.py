8ffbb080da7017efeada7f83ec875f4726702e0b
natisnowflake2023 <137405698+natisnowflake2023@users.noreply.github.com>
2023-06-26 13:35:47 -0400

    Update streamlit_app.py
    sd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas
 import streamlit
+import snowflake.connector
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
