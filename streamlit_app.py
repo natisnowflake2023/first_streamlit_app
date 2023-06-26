0a7f1066ed8eb313c7fab7b477b8ad08e521446e
natisnowflake2023 <137405698+natisnowflake2023@users.noreply.github.com>
2023-06-26 14:31:12 -0400

    Update streamlit_app.py
    cd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas
 import streamlit
-# import snowflake.connector
+import snowflake.connector
 
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # my_cur = my_cnx.cursor()
